295755fe67d74c2b5e5e10f2178bddc1e6ddf60b
brokenVectors <brokenVectors@gmail.com>
2022-09-04 16:19:37 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function AddForm(props) {
+	const [text, changeText] = useState("");
+	return <form onSubmit={(e) => {e.preventDefault(); props.onAdd(text)}}>
+		<input onChange={(e) => {changeText(e.target.value)}}></input>
+		<input type="submit" value="Add"/>
+	</form>
+}
+
+function TodoElement(props) {
+	return <div className="todoElement">
+		<p>{props.text}</p>
+		<button onClick={props.onRemove}>X</button>
+	</div>
+}
+
+function TodoList() {
+	const [todo, setTodo] = useState([]);
+	const [didLoad, setLoaded] = useState(false);
+	
+
+	let todoSave = localStorage.getItem('todo');
+	if(!didLoad && todoSave) {
+		setLoaded(true);
+		setTodo(JSON.parse(todoSave));
+	}
+	let save = () => {
+		localStorage.setItem('todo', JSON.stringify(todo));
+	}
+	let handleAdd = (text) => {
+		let newTodo = todo.map((x) => x);
+		newTodo.push(text);
+		setTodo(newTodo);
+	};
+	let handleRemove = (id) => {
+		let newTodo = todo.map((x) => x);
+		newTodo.splice(id, 1);
+		setTodo(newTodo);
+	}
+	useEffect(save, [todo]);
+	let elements = [];
+	for(let i = 0; i < todo.length; i++) {
+		elements.push(<TodoElement key={i} index={i} text={todo[i]} onRemove={() => {handleRemove(i)}} />);
+	}
+	return <div>
+		<h1>Todo List</h1>
+		{ elements }
+		<AddForm onAdd={handleAdd}/>
+	</div>
+}
+function Page(props) {
+	return <div>
+		<TodoList />
+	</div>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Page text="ok"/>);
